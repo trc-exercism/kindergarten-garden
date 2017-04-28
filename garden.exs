35510c7744ad93fa903ec1950f9e7a5114f07436
Alexander Teno <alexander.teno@gmail.com>
2017-04-28 15:28:13 -0400

    some setup

--- garden.exs
+++ garden.exs
@@ -1,4 +1,7 @@
 defmodule Garden do
+
+  @default_names ~w(alice bob charlie david eve fred ginny harriet leana joseph kincaid larry)a
+  @plants %{?G => :grass, ?C => :clover, ?V => :violets, ?R => :radishes}
   @doc """
     Accepts a string representing the arrangement of cups on a windowsill and a
     list with names of students in the class. The student names list does not
@@ -8,7 +11,14 @@ defmodule Garden do
     that information in a map.
   """
 
+  @spec info(String.t()) :: map
+  def info(info_string) do
+    info(info_string, @default_names)
+  end
+
   @spec info(String.t(), list) :: map
   def info(info_string, student_names) do
+    %{student_name => {}}
   end
+
 end
